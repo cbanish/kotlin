becbc6581ec071c7d9f0999c6432cd88469148fe
cbanish <cbanish.balakrishnan@gmail.com>
2024-05-01 11:27:57 +0530

    Kotlin Datatypes

---
+++ src/DataTypes.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val name:String ="Anish"
+    val age:Int = 36
+    val height:Double =5.9
+    val isStudent:Boolean=false
+    println("Name: $name, Age: $age, Height: $height, Student: $isStudent")
+}
